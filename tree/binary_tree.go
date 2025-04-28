f092e9e1c2b8ca46a4744942024e6a33501dc954
Aurelienbr96 <afrenchilearninglanguages@gmail.com>
2025-04-28 10:15:48 +0200

    feat: DFS

--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -1,5 +1,10 @@
 package tree
 
+import (
+	"fmt"
+	"math"
+)
+
 type Node[T any] struct {
 	Value T
 	Left  *Node[T]
@@ -26,6 +31,53 @@ func walk[T any](curr *Node[T], path *[]T) *[]T {
 	return path
 }
 
-func PrintTree[T any](head *Node[T]) *[]T {
+// depth first search, DFS
+func PreOrderSearch[T any](head *Node[T]) *[]T {
 	return walk(head, &[]T{})
+}
+
+func walkInOrderSearch[T any](curr *Node[T], path *[]T) *[]T {
+	if curr == nil {
+		return path
+	}
+
+	walkInOrderSearch(curr.Left, path)
+	*path = append(*path, curr.Value)
+	walkInOrderSearch(curr.Right, path)
+
+	return path
+}
+
+func InOrderSearch[T any](head *Node[T]) *[]T {
+	return walkInOrderSearch(head, &[]T{})
+}
+
+func walkPostOrderSearch[T any](curr *Node[T], path *[]T) *[]T {
+	if curr == nil {
+		return path
+	}
+
+	walkPostOrderSearch(curr.Left, path)
+	walkPostOrderSearch(curr.Right, path)
+	*path = append(*path, curr.Value)
+
+	return path
+}
+
+func PostOrderSearch[T any](head *Node[T]) *[]T {
+	return walkPostOrderSearch(head, &[]T{})
+}
+
+func TestLooseWeigh(weight float64, goalWeight float64, dept int32) string {
+	if goalWeight > weight {
+		return "Goal weigh too high"
+	}
+	if weight <= goalWeight {
+		return "goal achieve"
+	}
+	weightW := weight * 0.005
+	finalWeight := weight - weightW
+	fmt.Printf("(week %d) This week you should loose %vkg and weight %v \n", dept, math.Round(weightW*10)/10, math.Round(finalWeight*10)/10)
+	TestLooseWeigh(finalWeight, goalWeight, dept+1)
+	return ""
 }
--- tree/binary_tree_test.go
+++ tree/binary_tree_test.go
@@ -8,10 +8,10 @@ import (
 
 func TestStack(t *testing.T) {
 
-	t.Run("Test tree", func(t *testing.T) {
+	t.Run("PreOrderSearch", func(t *testing.T) {
 		node1 := NewNode(2, NewNode(3, NewNode(5, nil, nil), NewNode(15, nil, nil)), NewNode(20, nil, nil))
 
-		result := PrintTree(node1)
+		result := PreOrderSearch(node1)
 
 		expect := []int{
 			2,
@@ -22,6 +22,43 @@ func TestStack(t *testing.T) {
 		}
 
 		assert.Equal(t, result, expect)
+	})
+
+	t.Run("InOrderSearch", func(t *testing.T) {
+		node1 := NewNode(2, NewNode(3, NewNode(5, nil, nil), NewNode(15, nil, nil)), NewNode(20, nil, nil))
+
+		result := InOrderSearch(node1)
+
+		expect := []int{
+			5,
+			3,
+			15,
+			2,
+			20,
+		}
+
+		assert.Equal(t, result, expect)
+	})
+	t.Run("PostOrderSearch", func(t *testing.T) {
+		node1 := NewNode(2, NewNode(3, NewNode(5, nil, nil), NewNode(15, nil, nil)), NewNode(20, NewNode(1, nil, nil), NewNode(2, nil, nil)))
+
+		result := PostOrderSearch(node1)
+
+		expect := []int{
+			5,
+			15,
+			3,
+			1,
+			2,
+			20,
+			2,
+		}
+
+		assert.Equal(t, result, expect)
+	})
+	t.Run("TestLooseWeigh", func(t *testing.T) {
+		TestLooseWeigh(77, 70, 1)
+
 	})
 
 }
